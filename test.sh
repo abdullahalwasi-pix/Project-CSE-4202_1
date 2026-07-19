cd0fa5e60b0b1580da70be11db98e33ee605121a
Abdullah Al Wasi <abdullahalwasi@iut-dhaka.edu>
2026-07-20 01:51:31 +0600

    Added GPA tests

--- test.sh
+++ test.sh
@@ -15,3 +15,10 @@ chmod +x test_course_result.out
 if [ $? -ne 0 ]; then
     exit 1
 fi
+gcc -I modules tests/test_gpa.c modules/course.c modules/courseResult.c modules/gpa.c -o test_gpa.out
+chmod +x test_gpa.out
+./test_gpa.out
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
